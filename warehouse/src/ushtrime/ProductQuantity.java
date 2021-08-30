d3cd5d75568bc80d4911b0f1f54f2acbe02d6bf9
leonardoo1 <leonardlaci17@gmail.com>
2021-08-30 10:58:09 +0200

    Add files via upload

---
+++ warehouse/src/ushtrime/ProductQuantity.java
@@ -0,0 +1,11 @@
+package ushtrime;
+
+import Product;
+/**
+* Create a class called ProductQuantity to represent a product and the quantity
+* the quantity is a positive number.*/
+public class ProductQuantity {
+
+   Product prodtest = new Product();
+
+}
---
+++ warehouse/src/ushtrime/Warehouse.java
@@ -0,0 +1,16 @@
+package ushtrime;
+/**
+* Create a class called Warehouse that a warehouse might use to represent the actual storage
+in it
+* A warehouse should include the following information as instance variables:
+* storage, a map of all the products that the warehouse has (type HashMap<String,
+ProductQuantity>). The key is product’s description
+* In addition, provide a method named getProductQuantity(String description) that returns
+the product that is being searched if there is any in the warehouse. If the product is found
+in the warehouse, returns the ProductQuantity
+* Provide a method addProductQuantity(Product, Number) that saves input products to the
+warehouse storage. This method should take as inputs the Product and the quantity of
+product. It should be a void method and should add the product to warehouse storage map*/
+public class Warehouse {
+
+}
